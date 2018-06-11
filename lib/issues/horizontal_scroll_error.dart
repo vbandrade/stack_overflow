13ab099b37fd0292813fd47674d37f4e743311fa
@vbandrade <vbandrade@gmail.com>
2018-06-11 16:15:55 -0300

    criação de um arquivo de issues

---
+++ lib/issues/horizontal_scroll_error.dart
@@ -0,0 +1,42 @@
+// https://github.com/flutter/flutter/issues/18368
+
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MaterialApp(
+    title: 'stack overflow',
+    theme: ThemeData(
+      primarySwatch: Colors.pink,
+    ),
+    routes: {},
+    home: MyWidget(),
+  ));
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget tagList = Container(
+      color: Colors.green,
+      height: 40.0,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return Text("Text $index");
+        },
+      ),
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Test title"),
+        ),
+        body: Container(
+          color: Colors.grey[400],
+          child: tagList,
+          padding: EdgeInsets.all(20.0),
+          margin: EdgeInsets.all(20.0),
+        ));
+  }
+}
